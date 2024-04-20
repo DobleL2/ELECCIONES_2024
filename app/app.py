41968bcbb825fb969dfa55991f2be875b1fe3e42
DobleL2 <luis.lapo@epn.edu.ec>
2024-04-20 13:04:17 -0500

    updating connection and dockerfile

--- app/app.py
+++ app/app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import connection as connection
 
 """
 # Welcome to Streamlit!
@@ -35,3 +36,11 @@ with st.echo(code_location='below'):
    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
       .mark_circle(color='#0068c9', opacity=0.5)
       .encode(x='x:Q', y='y:Q'))
+   
+
+# SQL query
+query = "SELECT * FROM dbo.JUNTA LIMIT 100"
+
+# Execute query
+juntas = connection.query_function(query)
+st.write(juntas)
